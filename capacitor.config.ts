bc397127f12dfac42309a4b3bad8f617d581722b
Nikolay Popov <nikolay.popov@devpulse.io>
2024-07-04 17:48:18 +0300

    Added capacity/ios project

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.iExcalidraw.app',
+  appName: 'iExcalidraw',
+  webDir: 'excalidraw-app/build'
+};
+
+export default config;
